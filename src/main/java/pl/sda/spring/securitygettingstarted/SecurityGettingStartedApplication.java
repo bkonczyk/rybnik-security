5dd8256c250d76ee2bd968766fef1b1bdc7beaa2
bkonczyk <bartkon@gmail.com>
2022-09-24 12:36:29 +0200

    initial

---
+++ src/main/java/pl/sda/spring/securitygettingstarted/SecurityGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.securitygettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGettingStartedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/spring/securitygettingstarted/SecurityGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.securitygettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGettingStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
